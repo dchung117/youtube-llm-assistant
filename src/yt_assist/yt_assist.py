ec5e0bc9125216dbe7c3e64cf4ef222c735fad78
dchung117 <dchung0330@gmail.com>
2023-10-07 16:48:03 -0400

    installed tiktoken dependency for openaiembeddings.

--- src/yt_assist/yt_assist.py
+++ src/yt_assist/yt_assist.py
@@ -14,20 +14,32 @@ load_dotenv()
 
 EMBEDDINGS = OpenAIEmbeddings()
 
-def get_vectordb_from_youtube(url: str) -> VectorStore:
+def get_vectordb_from_url(url: str) -> VectorStore:
+    """
+    Get VectorStore database of Youtube transcript from video url.
+
+    Args
+    ----
+        url: str
+            Youtube video url
+    Return
+    ------
+        VectorStore
+            Vector database of embedded transcript documents.
+    """
     # Get transcript from video_url
     yt_loader = YoutubeLoader.from_youtube_url(url)
-    transcript = loader.load()
+    transcript = yt_loader.load()
 
     # Split transcript into documents
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=100
     )
-    docs = text_splitter.split_documents(transcripts)
-
+    docs = text_splitter.split_documents(transcript)
+    print(docs)
     # Create vector db from transript using OpenAI embeddings
     db = FAISS.from_documents(docs,
-        embeddings=EMBEDDINGS)
+        embedding=EMBEDDINGS)
 
     return db
