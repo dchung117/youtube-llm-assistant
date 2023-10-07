3bdfadfc9f848db7bb923cd189e44330c4ad8aef
dchung117 <dchung0330@gmail.com>
2023-10-07 16:31:58 -0400

    created vectorstore constructor function.

--- src/yt_assist/yt_assist.py
+++ src/yt_assist/yt_assist.py
@@ -1,5 +1,33 @@
-from dotenv import load_dotnev
+from dotenv import load_dotenv
+
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-load_dotnev()
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.vectorstores import FAISS, VectorStore
+
+load_dotenv()
+
+EMBEDDINGS = OpenAIEmbeddings()
+
+def get_vectordb_from_youtube(url: str) -> VectorStore:
+    # Get transcript from video_url
+    yt_loader = YoutubeLoader.from_youtube_url(url)
+    transcript = loader.load()
+
+    # Split transcript into documents
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100
+    )
+    docs = text_splitter.split_documents(transcripts)
+
+    # Create vector db from transript using OpenAI embeddings
+    db = FAISS.from_documents(docs,
+        embeddings=EMBEDDINGS)
+
+    return db
